9b1d68c3403ba9b3e9649ac1153e01ab03da1d1c
Pavel Petrov <pppetrv@gmail.com>
2026-08-17 13:09:35 +0300

    setup git actions workflow, added yaml file, project build fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,7 @@ android {
        debug {
             isMinifyEnabled = false
             isShrinkResources = false
+            signingConfig = signingConfigs.getByName("release")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
